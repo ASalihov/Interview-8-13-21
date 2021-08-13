45569245f235a5bad07bef242b84edabfa44cd04
Akmal Salikhov <akmal.salikhov@ya.ru>
2021-08-13 22:14:50 +0300

    FolderAnalyzer refactoring

--- Interview.Api/Domain/FolderAnalyzer.cs
+++ Interview.Api/Domain/FolderAnalyzer.cs
@@ -5,18 +5,19 @@ namespace Interview.Api.Domain
 {
     public class FolderAnalyzer : IFolderAnalyzer
     {
+        private const char separator = '.';
+
         public List<Folder> GetHangingFolders(List<Folder> folders)
         {
             List<Folder> hangingFolders = new();
             var folderMap = folders.ToDictionary(k => k.Index, v => v);
+
             foreach (var folder in folders)
             {
-                if (ParentExistIter(folderMap, folder.Index))
+                if (!ParentExistIter(folderMap, folder.Index))
                 {
-                    continue;
+                    hangingFolders.Add(folder);
                 }
-
-                hangingFolders.Add(folder);
             }
 
             return hangingFolders;
@@ -31,18 +32,30 @@ namespace Interview.Api.Domain
                 return false;
             }
 
-            if (!parentIndex.Contains('.'))
+            if (IsLastSegment(parentIndex))
             {
                 return true;
             }
 
-            var i = parentIndex.LastIndexOf('.');
-            if (parentIndex.Length == i + 1)
+            var lastSeparatorIndex = parentIndex.LastIndexOf(separator);
+            
+            if (IsIndexIncomplete(parentIndex, lastSeparatorIndex))
             {
                 return false;
             }
-            var nextParentIndex = parentIndex.Substring(0, i);
+
+            var nextParentIndex = parentIndex.Substring(0, lastSeparatorIndex);
             return ParentExistIter(folders, nextParentIndex);
+        }
+
+        private bool IsIndexIncomplete(string index, int lastSeparatorIndex)
+        {
+            return index.Length == lastSeparatorIndex + 1;
+        }
+
+        private bool IsLastSegment(string index)
+        {
+            return !index.Contains(separator);
         }
     }
 }
